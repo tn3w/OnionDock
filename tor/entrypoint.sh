8a4fa1d958bd542e43a3c9036085072a811389bc
tn3w <tn3w@protonmail.com>
2025-04-22 10:09:35 +0200

    Fix entrypoint.sh by removing unnecessary newline at the end of the file for cleaner script execution.

--- tor/entrypoint.sh
+++ tor/entrypoint.sh
@@ -6,4 +6,4 @@ chown -R tor:tor /var/lib/tor /var/lib/hidden_service
 chmod -R 700 /var/lib/tor /var/lib/hidden_service
 
 # Switch to tor user and run the original command
-exec su -s /bin/bash tor -c "/usr/local/bin/start.sh" 
+exec su -s /bin/bash tor -c "/usr/local/bin/start.sh"
