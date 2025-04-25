13132dd69d6093b8381c32c450d2e6d9a7fa3eea
tn3w <tn3w@protonmail.com>
2025-04-25 23:05:27 +0200

    Update entrypoint.sh to fix permissions for pluggable transports directory, ensuring proper access for the tor user. This enhances security and functionality of the Tor service.

--- tor/entrypoint.sh
+++ tor/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-# Fix permissions on mounted volumes
 chown -R tor:tor /var/lib/tor /var/lib/hidden_service
 chmod -R 700 /var/lib/tor /var/lib/hidden_service
 
-# Switch to tor user and run the original command
+chown -R tor:tor /usr/local/bin/pluggable_transports
+chmod -R 755 /usr/local/bin/pluggable_transports
+
 exec su -s /bin/bash tor -c "/usr/local/bin/start.sh"
