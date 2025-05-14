2ea83cdca1ff2fb106471e804fc0a17f46cd242f
tn3w <tn3w@protonmail.com>
2025-05-14 08:37:54 -0400

    Update Dockerfiles to include gcompat and libc6-compat for improved compatibility. Modify entrypoint.sh to adjust permissions for the /tmp directory, enhancing security and functionality of the Tor service. Update index.html to remove 'sudo' from the command for better user experience.

--- tor/entrypoint.sh
+++ tor/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-chown -R tor:tor /var/lib/tor /var/lib/hidden_service
+chown -R tor:tor /var/lib/tor /var/lib/hidden_service /tmp
 chmod -R 700 /var/lib/tor /var/lib/hidden_service
 
 chown -R tor:tor /usr/local/bin/pluggable_transports
 chmod -R 755 /usr/local/bin/pluggable_transports
+chmod 755 /usr/local/bin/pluggable_transports/*
 
 exec su -s /bin/bash tor -c "/usr/local/bin/start.sh"
